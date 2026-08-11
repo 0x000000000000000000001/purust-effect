5990861bfcb8a1d8afe7f19ae21e70bea41d0bc5
0x1 <kevin.francart3@gmail.com>
2026-08-11 21:22:16 +0200

    misc

--- src/Effect.rs
+++ src/Effect.rs
@@ -1,18 +1,18 @@
 pub fn Effect_pureE(a: crate::UnknownType) -> crate::UnknownType {
-    crate::UnknownType::new(std::rc::Rc::new(move |_: crate::UnknownType| -> crate::UnknownType {
+    crate::UnknownType::new(crate::Record_a { call: Some(std::rc::Rc::new(move |_: crate::UnknownType| -> crate::UnknownType {
         a.clone()
-    }))
+    })), ..Default::default() })
 }
 
 pub fn Effect_bindE(a: crate::UnknownType, f: crate::UnknownType) -> crate::UnknownType {
-    crate::UnknownType::new(std::rc::Rc::new(move |_: crate::UnknownType| -> crate::UnknownType {
+    crate::UnknownType::new(crate::Record_a { call: Some(std::rc::Rc::new(move |_: crate::UnknownType| -> crate::UnknownType {
         // a is an Effect, f is (a -> Effect b). Both are functions of Unit.
-        let a_fn = a.clone().into_rc_fn();
+        let a_fn = a.clone().call.clone().unwrap();
         let a_res = a_fn(crate::UnknownType::new(0));
-        let f_fn1 = f.clone().into_rc_fn();
+        let f_fn1 = f.clone().call.clone().unwrap();
         let b_eff = f_fn1(a_res);
-        let b_fn = b_eff.into_rc_fn();
+        let b_fn = b_eff.call.clone().unwrap();
         b_fn(crate::UnknownType::new(0))
-    }))
+    })), ..Default::default() })
 }
 pub type Effect = crate::UnknownType;
