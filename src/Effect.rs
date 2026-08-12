1d3be5d581e70975b5c00217f9a028fcb8ef96e8
0x1 <kevin.francart3@gmail.com>
2026-08-12 09:18:42 +0200

    misc

--- src/Effect.rs
+++ src/Effect.rs
@@ -8,11 +8,11 @@ pub fn Effect_bindE(a: crate::UnknownType, f: crate::UnknownType) -> crate::Unkn
     crate::UnknownType::new(crate::Record_a { call: Some(std::rc::Rc::new(move |_: crate::UnknownType| -> crate::UnknownType {
         // a is an Effect, f is (a -> Effect b). Both are functions of Unit.
         let a_fn = a.clone().call.clone().unwrap();
-        let a_res = a_fn(crate::UnknownType::new(0));
+        let a_res = a_fn(crate::UnknownType::new(Record_a { ..Default::default() }));
         let f_fn1 = f.clone().call.clone().unwrap();
         let b_eff = f_fn1(a_res);
         let b_fn = b_eff.call.clone().unwrap();
-        b_fn(crate::UnknownType::new(0))
+        b_fn(crate::UnknownType::new(Record_a { ..Default::default() }))
     })), ..Default::default() })
 }
 pub type Effect = crate::UnknownType;
--- src/Effect/Unsafe.rs
+++ src/Effect/Unsafe.rs
@@ -1,2 +1,2 @@
 
-pub fn Effect_Unsafe_unsafePerformEffect() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Effect_Unsafe_unsafePerformEffect() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
